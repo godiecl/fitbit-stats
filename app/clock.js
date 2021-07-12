948dfe5017584498a965b64e8e5556500c32d4ff
Diego Urrutia-Astorga <mika.cl@gmail.com>
2021-07-12 16:06:03 -0400

    refactor(Clean): docs + variables name

--- app/clock.js
+++ app/clock.js
@@ -9,8 +9,9 @@ const theHour = document.getElementById('clock-hour');
 const theSeparator = document.getElementById('clock-separator');
 const theMinutes = document.getElementById('clock-minutes');
 const theSeconds = document.getElementById('clock-seconds');
-const secHand = document.getElementById('secs');
+const theSecondsHand = document.getElementById('clock-seconds-hand');
 
+// The timer to move the seconds hand
 let theMillisTimer = null;
 
 export function initialize () {
@@ -19,37 +20,40 @@ export function initialize () {
 }
 
 export function onScreenOn () {
+  // Start the draw of time
   clock.addEventListener('tick', draw);
+
+  // Move the hand
   theMillisTimer = setInterval(() => {
     const date = new Date();
-    secHand.groupTransform.rotate.angle = 6 * date.getSeconds() + 0.006 * date.getMilliseconds();
+    theSecondsHand.groupTransform.rotate.angle = 6 * date.getSeconds() + 0.006 * date.getMilliseconds();
   }, 100);
 }
 
 export function onScreenOff () {
   clock.removeEventListener('tick', draw);
-  secHand.style.opacity = 0;
+  theSecondsHand.style.opacity = 0;
   clearInterval(theMillisTimer);
   theMillisTimer = null;
 }
 
 export function onPresent () {
   theClock.style.fill = 'fb-white';
+  theSeconds.style.fill = 'fb-red';
 }
 
 export function onAbsent () {
   theClock.style.fill = 'fb-dark-gray';
+  theSeconds.style.fill = 'fb-dark-gray';
 }
 
 function draw (evt) {
-  secHand.style.opacity = 1;
+  // Show the seconds hand
+  theSecondsHand.style.opacity = 1;
+  // Update the hour:minutes:seconds
   theSeconds.text = util.zeroPad(evt.date.getSeconds());
   theMinutes.text = util.zeroPad(evt.date.getMinutes());
   theHour.text = evt.date.getHours();
-  // `${hours}`;
+  // : on/off
   theSeparator.style.opacity ^= 1;
-  // console.log('Show: ' + theSeparator.style.display);
-  // const secs = evt.date.getSeconds();
-  // const secAngle = secondsToAngle(secs);
-  // secHand.groupTransform.rotate.angle = secAngle;
 }
