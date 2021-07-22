2fe46917f5a40dbe20dc7a4f48b6259b5e3db2b7
Diego Urrutia-Astorga <mika.cl@gmail.com>
2021-07-22 11:52:31 -0400

    feat(User): add the last sync time

--- app/user.js
+++ app/user.js
@@ -1,11 +1,36 @@
 /* eslint-disable import/no-unresolved */
 import document from 'document';
 import { user } from "user-profile";
+import { me as device } from "device";
 
 const theBmr = document.getElementById("user-bmr");
 const theBmi = document.getElementById("user-bmi");
 const theWeight = document.getElementById("user-weight");
 const theResting = document.getElementById("user-rhr");
+const theLastSync = document.getElementById("user-lastsync");
+
+function getTimeInterval(date) {
+    if (typeof date === 'undefined') {
+        return "--";
+    }
+    const seconds = Math.floor((Date.now() - date) / 1000);
+    let unit = "s";
+    let value = seconds;
+    if (seconds >= 31536000) {
+        value = Math.floor(seconds / 31536000);
+        unit = "y";
+    } else if (seconds >= 86400) {
+        value = Math.floor(seconds / 86400);
+        unit = "d";
+    } else if (seconds >= 3600) {
+        value = Math.floor(seconds / 3600);
+        unit = "h";
+    } else if (seconds >= 60) {
+        value = Math.floor(seconds / 60);
+        unit = "m";
+    }
+    return `${value}${unit}`;
+}
 
 export function initialize() {
 }
@@ -15,25 +40,14 @@ export function onScreenOn() {
     theBmi.text = (user.weight / (user.height * user.height)).toFixed(1);
     theWeight.text = user.weight;
     theResting.text = user.restingHeartRate;
+    theLastSync.text = getTimeInterval(device.lastSyncTime);
 }
 
 export function onScreenOff() {
 }
 
 export function onPresent() {
-    /*
-    theBmr.style.fill = 'fb-white';
-    theBmi.style.fill = 'fb-white';
-    theWeight.style.fill = 'fb-white';
-    theResting.style.fill = 'fb-white';
-    */
 }
 
 export function onAbsent() {
-    /*
-    theBmr.style.fill = 'fb-dark-gray';
-    theBmi.style.fill = 'fb-dark-gray';
-    theWeight.style.fill = 'fb-dark-gray';
-    theResting.style.fill = 'fb-dark-gray';
-    */
 }
